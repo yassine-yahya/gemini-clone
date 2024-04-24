178d1d6a57f70e382cf6931bb6f180019456aa40
Dev271181 <127858793+Dev271181@users.noreply.github.com>
2024-04-24 13:38:27 +0200

    main and sidebar component in process

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Sidebar from "./components/Sidebar/Sidebar"
+import Main from "./components/Main/Main"
+
+const App = () => {
+  return (
+    <>
+<Sidebar/>
+<Main/>
+
+    </>
+  )
+}
+
+export default App
